242a52d74c4ea967d5844c75162b34b7dff7c3a2
sairanjith101 <sairanjith101@gmail.com>
2025-05-12 19:43:22 +0530

    add

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {"data": 'blog list'}
+
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -20,7 +30,19 @@ def comments(id):
     # fetch comments of blog with id = id
     return {'data': {'1', '2'}}
 
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    # fetch comments of blog with id = id
+    return {'data': {'1', '2'}}
 
+class Blog(BaseModel):
+    title: str
+    body: str
+    publoshed: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+    return {'data': f"Blog is created with title as {blog.title}"}
 
 # uvicorn main:app ==> uvicorn filename.py(main):object(app)
 # @app - path operation decorator
