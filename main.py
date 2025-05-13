3aa981d93d6b334b48fb2b016a5543a4cbc0e38c
sairanjith101 <sairanjith101@gmail.com>
2025-05-13 16:03:32 +0530

    add

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -43,6 +44,10 @@ class Blog(BaseModel):
 @app.post('/blog')
 def create_blog(blog:Blog):
     return {'data': f"Blog is created with title as {blog.title}"}
+
+# Chnage port 8000 to 9000
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port= 9000)
 
 # uvicorn main:app ==> uvicorn filename.py(main):object(app)
 # @app - path operation decorator
